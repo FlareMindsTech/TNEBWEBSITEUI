338205990d1ebd5e92ae94e15e6ac0f643ac8b6a
msdhinesh45 <dhineshkumar111202@gmail.com>
2026-02-12 11:53:42 +0530

    proposed buildings updated

--- src/pages/Minnagam.js
+++ src/pages/Minnagam.js
@@ -65,9 +65,9 @@ const ContactCard = ({ contact, index }) => {
 
 const Minnagam = () => {
   const buildingImages = [
-    { id: 1, src: building1, alt: 'Proposed Building - View 1' },
+    { id: 1, src: building2, alt: 'Proposed Building - View 1' },
     { id: 2, src: building3, alt: 'Proposed Building - View 2' },
-    { id: 3, src: building2, alt: 'Proposed Building - View 3' }
+    { id: 3, src: building1, alt: 'Proposed Building - View 3' }
   ];
 
   const rooms = [
